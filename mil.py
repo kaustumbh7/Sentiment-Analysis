1923e2781037ebe83a25de06a9132816cf882255
kaustumbh7 <32202423+kaustumbh7@users.noreply.github.com>
2017-11-14 21:22:00 +0530

    Add files via upload

---
+++ mil.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+
+import speech_recognition as sr
+
+# get audio from the microphone
+r=sr.Recognizer()
+with sr.Microphone() as source:
+	print("Speak:")
+	audio=r.listen(source)
+
+try:
+	f=open('speech2text.txt','w')
+	f.write('{ "name": "' + r.recognize_google(audio)+'" }')
+	f.close()
+	print('{ "name": "' + r.recognize_google(audio)+'" }')
+except sr.UnknownValueError:
+	print("Could not understand audio")
+except sr.RequestError as e:
+	print("Could not request results; {0}".format(e))
+
+
+
+
+
+# -*- coding: utf-8 -*-
+
+import httplib, urllib
+import json
+
+# **********************************************
+# *** Update or verify the following values. ***
+# **********************************************
+
+# Replace the accessKey string value with your valid access key.
+accessKey = '70497ccef8024ce888ca11aa6d56df0b'
+
+# Replace or verify the region.
+#
+# You must use the same region in your REST API call as you used to obtain your access keys.
+# For example, if you obtained your access keys from the westus region, replace 
+# "westcentralus" in the URI below with "westus".
+#
+# NOTE: Free trial access keys are generated in the westcentralus region, so if you are using
+# a free trial access key, you should not need to change this region.
+uri = 'westcentralus.api.cognitive.microsoft.com'
+path = '/text/analytics/v2.0/sentiment'
+
+def GetSentiment (documents):
+    "Gets the sentiments for a set of documents and returns the information."
+
+    headers = {'Ocp-Apim-Subscription-Key': accessKey}
+    conn = httplib.HTTPSConnection (uri)
+    body = json.dumps (documents)
+    conn.request ("POST", path, body, headers)
+    response = conn.getresponse ()
+    return response.read ()
+
+f=open("speech2text.txt","r")
+
+documents = { 'documents': [
+    { 'id': '1', 'language': 'en', 'text': f.read()}
+]}
+
+f.close()
+
+print 'Please wait a moment for the results to appear.\n'
+
+result = GetSentiment (documents)
+print (json.dumps(json.loads(result), indent=4))
+
+
+
+
+
+# Import the required module for text 
+# to speech conversion
+from gtts import gTTS
+ 
+# This module is imported so that we can 
+# play the converted audio
+import os
+ 
+# The text that you want to convert to audio
+f=open("speech2text.txt","r")
+mytext = 'Thank you for your response! have a nice day!!'
+ 
+# Language in which you want to convert
+language = 'en'
+ 
+# Passing the text and language to the engine, 
+# here we have marked slow=False. Which tells 
+# the module that the converted audio should 
+# have a high speed
+myobj = gTTS(text=mytext, lang=language, slow=False)
+ 
+# Saving the converted audio in a mp3 file named
+# welcome 
+myobj.save("welcome.mp3")
+ 
+# Playing the converted file
+os.system("mpg321 welcome.mp3")
+f.close()
